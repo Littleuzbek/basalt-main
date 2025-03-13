7308340073c3533be4017800ccccae2613a9884b
Dilshodbek Akbarov <dilshod.akbarov.2003@gmail.com>
2025-03-14 01:20:20 +0500

    update

--- src/components/homeComponents/Team.jsx
+++ src/components/homeComponents/Team.jsx
@@ -1,16 +1,18 @@
 import "./Home.css"
 import historic from "../../assets/historic.mp4"
+import historicRus from "../../assets/historicRus.mp4"
 import hictoricImg from "../../assets/historicImg.png"
 import { useParams } from "react-router";
 
 export default function Team() {
   const {lang} = useParams();
+  const vid = lang === "en" ? historic : historicRus
   return (
     <div className="team" >
       <h2>{lang === "en" ? "The Company With History" : "Компания с Историей"}</h2>
       <span></span>
       <div>
-        <video src={historic} controls preload="auto" poster={hictoricImg}></video>
+        <video src={vid} controls preload="auto" poster={hictoricImg}></video>
       </div>
     </div>
   );
